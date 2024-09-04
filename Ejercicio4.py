c5e6dd1112344e834ed59ce8b8dc3f33664d8a69
Luciano Ismael Espejo <64952912+LucianoEspejo42@users.noreply.github.com>
2024-09-04 01:07:50 -0300

    Update Ejercicio4.py

--- Ejercicio4.py
+++ Ejercicio4.py
@@ -18,52 +18,85 @@ from tkinter import messagebox
 import numpy as np
 import math
 
+SALTO = 100
+
 
 def calcular_capacidad(matriz, tamanio_canal):
     try:
         maxima_entropia = 0
-        probabilidad1 = 0
-        probabilidad2 = 0
-        for x in range(0, 100):
-            probabilidades_entrada=[x/100, 1-x/100]
-            
-            #creo las probabilidades de entrada
-            #for i in range(tamanio_canal):
-             #   probabilidades_entrada.append(1/tamanio_canal)
-                
-            #calculo capacidad de canal
-            capacidad_canal=0      
-            entropia_salida=0 #H(B)
-            entropia_equivocacion=0 #H(B/A)
-            probabilidades_salida=[] #p(bj)
-            probabilidades_condicionales=[] #p(bj/ai)
-            
-            #calculo H(B)
-            for i in range(tamanio_canal):
-                pb=0
-                for j in range(tamanio_canal):
-                    if(matriz[j][i]!= 0):#verificar que p(bj/ai) no sea 0
-                        pb+=probabilidades_entrada[j]*matriz[j][i]
-                probabilidades_salida.append(pb)
-                entropia_salida+= pb*math.log2(1/pb)
+        probabilidades = []
+        probabilidades_entrada = []
+        cuaternario = 1
+        ternario = 1
+        binario = 1
+        if tamanio_canal == 4:
+            cuaternario = SALTO
+            ternario = SALTO
+            binario = SALTO
+        elif tamanio_canal == 3:
+            ternario = SALTO
+            binario = SALTO
+        elif tamanio_canal == 2:
+            binario = SALTO
+        else:
+            raise Exception
+        for z in range (0, cuaternario):
+            p3 = z / SALTO
+            for y in range (0, ternario-z):
+                p2 = y/SALTO
+                for x in range(0, binario-z-y):
+                    p1 = x/SALTO
+                    p4 = 1-p1
+                    
+                    probabilidades_entrada.clear()
+                    if cuaternario == SALTO:
+                        probabilidades_entrada.append(p3)
+                        p4-= p3
+                    if ternario == SALTO:
+                        probabilidades_entrada.append(p2)
+                        p4-= p2
+                    probabilidades_entrada.append(p1)
+                    probabilidades_entrada.append(p4)
 
-            #calculo H(B/A)
+
+                    #calculo capacidad de canal
+                    capacidad_canal=0      
+                    entropia_salida=0 #H(B)
+                    entropia_equivocacion=0 #H(B/A)
+                    probabilidades_salida=[] #p(bj)
+                    probabilidades_condicionales=[] #p(bj/ai)
+
+                    #calculo H(B)
+                    for i in range(tamanio_canal):
+                        pb=0
+                        for j in range(tamanio_canal):
+                            if(matriz[j][i]!= 0):#verificar que p(bj/ai) no sea 0
+                                pb+=probabilidades_entrada[j]*matriz[j][i]
+                        probabilidades_salida.append(pb)
+                        if pb !=0:
+                            entropia_salida+= pb*math.log2(1/pb)
+
+                    #calculo H(B/A)
+                    for i in range(tamanio_canal):
+                        pcondicional=0
+                        for j in range(tamanio_canal):
+                            if(matriz[i][j]!= 0):#verificar que p(bj/ai) no sea 0
+                                pcondicional+=probabilidades_entrada[i]*matriz[i][j]*math.log2(1/matriz[i][j])
+                        probabilidades_condicionales.append(pcondicional)
+                        entropia_equivocacion+= pcondicional
+
+                    #I(A,B)= H(B)-H(B/A) con p(ai) equiprobables se obtiene la informacion maxima que es la capacidad del canal
+                    capacidad_canal = entropia_salida-entropia_equivocacion
+                    if maxima_entropia<capacidad_canal:
+                        maxima_entropia=capacidad_canal
+                        probabilidades = probabilidades_entrada.copy()
+        if len(probabilidades) == 0:
             for i in range(tamanio_canal):
-                pcondicional=0
-                for j in range(tamanio_canal):
-                    if(matriz[i][j]!= 0):#verificar que p(bj/ai) no sea 0
-                        pcondicional+=probabilidades_entrada[i]*matriz[i][j]*math.log2(1/matriz[i][j])
-                probabilidades_condicionales.append(pcondicional)
-                entropia_equivocacion+= pcondicional
-            
-            #I(A,B)= H(B)-H(B/A) con p(ai) equiprobables se obtiene la informacion maxima que es la capacidad del canal
-            capacidad_canal = entropia_salida-entropia_equivocacion
-            if maxima_entropia<capacidad_canal:
-                maxima_entropia=capacidad_canal
-                probabilidad1= probabilidades_entrada[0]
-                probabilidad2 = probabilidades_entrada[1]
-        
-        messagebox.showinfo("Resultado", f"Las probabilidades que maximizan la capacidad de canal son: {probabilidad1, probabilidad2}\nLa capacidad de canal es: {maxima_entropia}")
+                probabilidades.append(0)
+                
+        probabilidades_redondeadas = [round(p, 5) for p in probabilidades]
+
+        messagebox.showinfo("Resultado", f"Las probabilidades que maximizan la capacidad de canal son: {probabilidades_redondeadas}\nLa capacidad de canal es: {maxima_entropia}")
     except ZeroDivisionError as z:
         messagebox.showerror("Error", f"Error en los valores ingresados: {z}")  
 def main (entry_matrix):
@@ -86,7 +119,7 @@ def main (entry_matrix):
         sumas_filas = np.sum(matriz_numpy, axis=1)  # axis=1 indica que la suma es por filas
         
         # Verificar si cada suma es aproximadamente 1 (usamos un margen de error para evitar problemas de precisión)
-        todas_filas_suman_uno = np.allclose(sumas_filas, np.ones(sumas_filas.shape), atol=1e-8)
+        todas_filas_suman_uno = np.allclose(sumas_filas, np.ones(sumas_filas.shape), atol=1e-3)
         
         if not (todas_filas_suman_uno):
             raise ValueError("Cada fila debe sumar 1.")
